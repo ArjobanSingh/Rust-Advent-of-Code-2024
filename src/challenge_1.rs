f6e45f4d63295a847ed5b44c75cce43576d67d58
ArjobanSingh <arjobansingh1998@gmail.com>
2024-12-01 21:28:42 +0530

    modify challenge 1's code to solve 2nd problem

--- src/challenge_1.rs
+++ src/challenge_1.rs
@@ -1,28 +1,28 @@
-use std::fs;
-
+use std::{collections::HashMap, fs};
+// 1666427
 pub fn get_distance_bw_lists(file_path: &str) {
-    let mut list_1: Vec<i32> = Vec::new();
-    let mut list_2: Vec<i32> = Vec::new();
+    let mut list_1: Vec<u32> = Vec::new();
+    let mut list_2_map: HashMap<u32, u32> = HashMap::new();
 
     if let Ok(data) = fs::read_to_string(file_path) {
         for (index, value) in data.split_whitespace().enumerate() {
             // In our case we'll only have two lists split over whitespace.
-            // and we safely unwrap knowing it will be converted to i32
             if index % 2 == 0 {
+                // We can safely unwrap knowing it will be converted to i32
                 list_1.push(value.parse().unwrap());
             } else {
-                list_2.push(value.parse().unwrap());
+                list_2_map
+                    .entry(value.parse().unwrap())
+                    .and_modify(|count| *count += 1)
+                    .or_insert(1);
             };
         }
     }
 
-    list_1.sort_unstable();
-    list_2.sort_unstable();
-
     let mut distance = 0;
-    list_1.iter().enumerate().for_each(|(idx, &value)| {
-        if let Some(&other) = list_2.get(idx) {
-            distance += value.abs_diff(other);
+    list_1.iter().for_each(|num| {
+        if let Some(&count) = list_2_map.get(num) {
+            distance += num * count;
         }
     });
     println!("The solution for 1st challenge is: {distance}");
