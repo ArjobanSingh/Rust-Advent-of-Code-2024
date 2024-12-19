c3b56fa65c38f810c1fcaac782ea098fac65bbdd
ArjobanSingh <arjobansingh1998@gmail.com>
2024-12-19 20:54:58 +0530

    solve challenge 13 and 14 puz 1

--- src/challenge_13.rs
+++ src/challenge_13.rs
@@ -5,14 +5,14 @@ use std::{
     io::{self, BufRead},
 };
 
-const COST_A: u8 = 3;
-const COST_B: u8 = 1;
-const NUM_TO_ADD: u64 = 10_000_000_000_000;
+const COST_A: i64 = 3;
+const COST_B: i64 = 1;
+const NUM_TO_ADD: i64 = 10_000_000_000_000;
 
 #[derive(Debug, Clone, Copy)]
 struct Coord {
-    x: u64,
-    y: u64,
+    x: i64,
+    y: i64,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -22,45 +22,33 @@ struct Game {
     result: Coord,
 }
 
-fn get_min_comb(game: &Game) -> Option<u64> {
-    let mut ans: Option<u64> = None;
-    let min_x = cmp::min(game.btn_a.x, game.btn_b.x);
-    let min_y = cmp::min(game.btn_a.y, game.btn_b.y);
+fn get_min_comb(game: &Game) -> Option<i64> {
+    let mut ans: Option<i64> = None;
+    let Game {
+        btn_a,
+        btn_b,
+        result,
+    } = game;
 
-    for count in 0.. {
-        let a_press_count = count;
-        let mut curr = Coord {
-            x: game.btn_a.x * a_press_count,
-            y: game.btn_a.y * a_press_count,
-        };
-        let mut b_press_count = 0;
+    let ax = btn_a.x as f64;
+    let ay = btn_a.y as f64;
+    let bx = btn_b.x as f64;
+    let by = btn_b.y as f64;
+    let px = result.x as f64;
+    let py = result.y as f64;
 
-        while curr.x < game.result.x && curr.y < game.result.y {
-            curr.x += game.btn_b.x;
-            curr.y += game.btn_b.y;
-            b_press_count += 1;
-        }
+    // Used Linear Algebra to find count of A and count of B pressess, on coniditon that two lines are not parallel
+    let ca = (px * by - py * bx) / (ax * by - ay * bx);
+    let cb = (px - ax * ca) / bx;
+    println!("ca and cb {ca} {cb}");
 
-        if curr.x == game.result.x && curr.y == game.result.y {
-            let tokens_count =
-                (a_press_count * (COST_A as u64)) + (b_press_count * (COST_B as u64));
-            if let Some(prev_ans) = ans {
-                ans = Some(cmp::min(prev_ans, tokens_count));
-            } else {
-                ans = Some(tokens_count);
-            }
-        }
-
-        // kind of hacky way to keep iterating till the smalles x and y deltas go across the result
-        // TODO: improve this logic
-        if min_x * count > game.result.x || min_y * count > game.result.y {
-            break;
-        }
+    if ca.fract() == 0.0 && cb.fract() == 0.0 {
+        ans = Some((ca as i64 * COST_A) + (cb as i64 * COST_B));
     }
     ans
 }
 
-pub fn min_tokens_1(file_path: &str) {
+pub fn min_tokens(file_path: &str) {
     if let Ok(file) = File::open(file_path) {
         let re = Regex::new(r"\d+").unwrap();
         let mut result = 0;
@@ -75,7 +63,7 @@ pub fn min_tokens_1(file_path: &str) {
                 continue;
             }
 
-            let nums: Vec<u64> = re
+            let nums: Vec<i64> = re
                 .captures_iter(&line)
                 .filter_map(|cap| cap.get(0)?.as_str().parse().ok()) // Only parse valid numbers
                 .collect();
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use challenge_10::topographic_map_search;
 use challenge_11::stone_count;
 use challenge_12::garden_groups;
 use challenge_12_puz_1::garden_groups_v1;
-use challenge_13_puz_1::min_tokens;
+use challenge_13::min_tokens;
+use challenge_13_puz_1::min_tokens_1;
+use challenge_14::restroom_redoubt;
 use challenge_2::get_safe_reports_count;
 use challenge_3::get_uncorrupted_mul_ans;
 // use challenge_4::search_for_xmas;
@@ -22,7 +24,9 @@ mod challenge_11;
 mod challenge_11_puz_1;
 mod challenge_12;
 mod challenge_12_puz_1;
+mod challenge_13;
 mod challenge_13_puz_1;
+mod challenge_14;
 mod challenge_2;
 mod challenge_3;
 mod challenge_4;
@@ -53,5 +57,6 @@ fn main() {
     // garden_groups("src/inputs/challenge_12.txt");
     // garden_groups("src/inputs/challenge_12.txt");
     // garden_groups("src/inputs/challenge_12.txt");
-    min_tokens("src/inputs/challenge_13.txt");
+    // min_tokens("src/inputs/challenge_13.txt");
+    restroom_redoubt("src/inputs/challenge_14.txt");
 }
