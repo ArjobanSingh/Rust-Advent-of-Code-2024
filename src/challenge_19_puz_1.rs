dc79d87d98801f5cbdd9c320e27d670c4d573606
ArjobanSingh <arjobansingh1998@gmail.com>
2024-12-27 01:06:50 +0530

    solve challenge 19 puz 2, small variation in dp.

--- src/challenge_19_puz_1.rs
+++ src/challenge_19_puz_1.rs
@@ -4,21 +4,20 @@ use std::{
     io::{self, BufRead},
 };
 
-// Simple for every char at i(0 to end), check at how many different ways, the substrings
-// can be created at idx j(subtring of 0..=j), where j goes from 0..i, such that str[j..i] is also available in patterns.
-fn can_create(design: &String, patterns: &HashSet<String>) -> i64 {
-    let mut dp: Vec<i64> = vec![0; design.len() + 1];
-    dp[0] = 1;
+fn can_create(design: &String, patterns: &HashSet<String>) -> bool {
+    let mut dp = vec![false; design.len() + 1];
+    dp[0] = true;
 
     for i in 1..=design.len() {
         for j in 0..i {
-            if dp[j] != 0 && patterns.get(&design[j..i]).is_some() {
-                dp[i] += dp[j];
+            if dp[j] && patterns.get(&design[j..i]).is_some() {
+                dp[i] = true;
+                break;
             }
         }
     }
 
-    *dp.last().unwrap_or(&0)
+    *dp.last().unwrap_or(&false)
 }
 
 pub fn linen_layout(file_path: &str) {
@@ -41,8 +40,10 @@ pub fn linen_layout(file_path: &str) {
                     patterns.insert(pattern);
                 }
             } else {
-                let possible_count = can_create(&line, &patterns);
-                result += possible_count;
+                let is_possible = can_create(&line, &patterns);
+                if is_possible {
+                    result += 1;
+                }
             }
         }
 
